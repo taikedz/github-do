d56e644b8d4a096fe939185f5d0745f3fe91ae56
Tai Kedzierski <dch.tai@gmail.com>
2018-07-19 12:41:52 +0100

    Get credentials at first curl run

--- src/ghcurl.sh
+++ src/ghcurl.sh
@@ -1,5 +1,14 @@
+#%include askuser.sh
+
 gh:curl() {
+    gh:getcreds
     ( set -x
     curl -s -S -u "$GHUSER:$GHTOKEN" "$@"
     )
+}
+
+gh:getcreds() {
+    [[ -n "${GHUSER:-}" ]] || GHUSER="$(askuser:ask "Github user name")"
+    [[ -n "${GHTOKEN:-}" ]] || GHTOKEN="$(askuser:password "Github Personal Access Token"; echo >&2)"
+    export GHUSER GHTOKEN
 }
